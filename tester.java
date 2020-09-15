d5fe7269540798cbd79dae893bfa41b4d1121f52
IgnacioNiveyro <nacho_niveyro@hotmail.com>
2020-09-15 18:59:37 -0300

    segundo commit

--- tester.java
+++ tester.java
@@ -1,6 +1,6 @@
 class tester{
 	public static void main(String[] args){
-		Guitarra g=new Guitarra(5);
+		Guitarra g=new Guitarra(6);
 		System.out.println("Cantidad de cuerdas: "+g.getCuerdas());
 	}
 }
