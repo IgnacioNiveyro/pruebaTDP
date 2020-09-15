3f9d3b4060ca58df6116f22ca4b89dfcfdfbd254
IgnacioNiveyro <nacho_niveyro@hotmail.com>
2020-09-15 18:47:48 -0300

    commit inicial

---
+++ Guitarra.java
@@ -0,0 +1,12 @@
+class Guitarra{
+	protected int cantCuerdas;
+	public Guitarra(int cant){
+		cantCuerdas=cant;
+	}
+	public void setCantCuerdas(int cant){
+		cantCuerdas=cant;
+	}
+	public int getCantCuerdas(){
+		return cantCuerdas;
+	}
+}
---
+++ tester.java
@@ -0,0 +1,6 @@
+class tester{
+	public static void main(String[] args){
+		Guitarra g=new Guitarra(5);
+		System.out.println("Cantidad de cuerdas: "+g.getCuerdas());
+	}
+}
