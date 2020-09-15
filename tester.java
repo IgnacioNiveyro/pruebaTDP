ab7212547c350d13ea4c65bc6811c6648e01774f
IgnacioNiveyro <nacho_niveyro@hotmail.com>
2020-09-15 19:02:04 -0300

    tercer commit

--- tester.java
+++ tester.java
@@ -1,6 +1,6 @@
 class tester{
 	public static void main(String[] args){
-		Guitarra g=new Guitarra(5);
+		Guitarra g=new Guitarra(4);
 		System.out.println("Cantidad de cuerdas: "+g.getCuerdas());
 	}
 }
